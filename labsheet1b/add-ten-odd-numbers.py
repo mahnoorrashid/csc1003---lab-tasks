b904cbc7e0f92f68d8176b950e3f6e22eecd925a
Mahnoor Rashid <147548022+mahnoorrashid@users.noreply.github.com>
2025-12-01 19:06:00 +0000

    Add script to sum ten odd numbers from input
    This script takes ten integer inputs and calculates the sum of the odd numbers among them.

---
+++ labsheet1b/add-ten-odd-numbers.py
@@ -0,0 +1,27 @@
+1 #!/usr/bin/env python3
+2 
+3 total = 0
+4 
+5 a = int(input())
+6 b = int(input())
+7 c = int(input())
+8 d = int(input())
+9 e = int(input())
+10 f = int(input())
+11 g = int(input())
+12 h = int(input())
+13 i = int(input())
+14 j = int(input())
+15 
+16 total = total + a % 2 * a
+17 total = total + b % 2 * b
+18 total = total + c % 2 * c
+19 total = total + d % 2 * d
+20 total = total + e % 2 * e
+21 total = total + f % 2 * f
+22 total = total + g % 2 * g
+23 total = total + h % 2 * h
+24 total = total + i % 2 * i
+25 total = total + j % 2 * j
+26 
+27 print(total)
