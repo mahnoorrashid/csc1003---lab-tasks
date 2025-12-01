4c38aab2f41a6a81f08f9da6f089cd89a8fa08c4
Mahnoor Rashid <147548022+mahnoorrashid@users.noreply.github.com>
2025-12-01 19:01:28 +0000

    Add script to sum ten user-input numbers

---
+++ labsheet1b/add-ten-numbers.py
@@ -0,0 +1,14 @@
+1 #!/usr/bin/env python3
+2 
+3 a = int(input())
+4 b = int(input())
+5 c = int(input())
+6 d = int(input())
+7 e = int(input())
+8 f = int(input())
+9 g = int(input())
+10 h = int(input())
+11 i = int(input())
+12 j = int(input())
+13 
+14 print(a + b + c + d + e + f + g + h + i + j)
