b1da56175194cd5fc96e8ac8eb45fc0dd1df63c3
Mahnoor Rashid <147548022+mahnoorrashid@users.noreply.github.com>
2025-12-01 19:02:57 +0000

    Add area calculation for rectangle in Python

---
+++ labsheet1b/area-of-rectangle.py
@@ -0,0 +1,13 @@
+1 #!/usr/bin/env python3
+2 
+3 x1 = int(input())
+4 y1 = int(input())
+5 x2 = int(input())
+6 y2 = int(input())
+7 
+8 length = x2 - x1
+9 width = y2 - y1
+10 
+11 area = length * width
+12 
+13 print(area)
