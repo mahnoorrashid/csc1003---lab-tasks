e0559de039a3fdf80101437f595dadcee28ec896
Mahnoor Rashid <147548022+mahnoorrashid@users.noreply.github.com>
2025-12-01 19:12:03 +0000

    Add script to create a three-digit number from input

---
+++ labsheet1c/three-digit-number.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+first_digit = input()
+second_digit = input()
+third_digit = input()
+
+three_digit_number = first_digit + second_digit + third_digit
+print(three_digit_number)
