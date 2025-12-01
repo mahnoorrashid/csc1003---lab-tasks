3955a13e624725f0b2824b7d1a4bf8d12c062d83
Mahnoor Rashid <147548022+mahnoorrashid@users.noreply.github.com>
2025-12-01 19:02:01 +0000

    Add script to sum ten numbers from user input
    This script takes ten integer inputs from the user and calculates their total.

---
+++ labsheet1b/add-ten-numbers-again.py
@@ -0,0 +1,34 @@
+1 #!/usr/bin/env python3
+2 
+3 total = 0
+4 
+5 num = int(input())
+6 total = total + num
+7 
+8 num = int(input())
+9 total = total + num
+10 
+11 num = int(input())
+12 total = total + num
+13 
+14 num = int(input())
+15 total = total + num
+16 
+17 num = int(input())
+18 total = total + num
+19 
+20 num = int(input())
+21 total = total + num
+22 
+23 num = int(input())
+24 total = total + num
+25 
+26 num = int(input())
+27 total = total + num
+28 
+29 num = int(input())
+30 total = total + num
+31 
+32 num = int(input())
+33 total = total + num
+34 print(total)
